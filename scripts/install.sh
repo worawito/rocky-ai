0554410afc092314c8aa00b353d66b2e0847c9f2
WorawitO <33147910+worawito@users.noreply.github.com>
2025-04-04 12:25:17 +0700

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Example install script
+echo "Starting Installation"
+# if you want to use kickstart from a local folder mounted to the ISO.
+# inst.ks=cdrom:/kickstart.cfg
+# if you want to use kickstart from a web server.
+# inst.ks=http://192.168.1.10/kickstart.cfg
+# This script would be run from the grub prompt.
+echo "Installation complete"
